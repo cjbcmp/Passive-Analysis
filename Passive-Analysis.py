aa605b9728be6345df911917cb7430917384e038
cjbcmp <cjbcmp@gmail.com>
2025-09-07 11:29:45 +0800

    Update Passive-Analysis.py

--- Passive-Analysis.py
+++ Passive-Analysis.py
@@ -294,9 +294,12 @@ def analyze_and_generate_kline_charts(filename, custom_params=None):
 
     output_filename = "low_volatility_stocks.xlsx"
     
-    # 创建新的列名，直接使用筛选后的股票代码（已确保前导零）
+    # 提取股票代码中的数字部分，并保持前导零
+    numeric_codes = [re.search(r'\d+', code).group(0) for code in valid_stocks]
+    
+    # 创建新的列名
     new_column_name = f"{latest_date_str}股票代码"
-    result_df = pd.DataFrame({new_column_name: valid_stocks})
+    result_df = pd.DataFrame({new_column_name: numeric_codes})
 
     # 使用 'xlsxwriter' 引擎并设置文本格式来保存
     with pd.ExcelWriter(output_filename, engine='xlsxwriter') as writer:
